e248f2f8399e577e161a65eb0030f903b8bd6438
abbydiode <contact@abbydiode.com>
2023-06-20 01:17:15 +0200

    Update create-version.test.ts

--- test/create-version.test.ts
+++ test/create-version.test.ts
@@ -11,16 +11,16 @@ const minute = String(now.getMinutes()).padStart(2, '0');
 const second = String(now.getSeconds()).padStart(2, '0');
 const formattedDate = `${year}-${month}-${day}, ${hour}:${minute}:${second}`;
 
-Assert.strictEqual(await Publish({
-    version_name: `Debug Version ${formattedDate}`,
-    version_number: '1.0.0',
-    changelog: 'Debug Changelog',
-    dependencies: '',
-    game_versions: '1.19, 1.19.1, 1.19.2, 1.19.3, 1.19.4',
-    version_type: 'release',
-    loaders: 'fabric',
-    featured: 'false',
-    status: 'listed',
-    project_id: process.env.DEBUG_PROJECT_ID,
-    file_path: './test/debug.zip',
-}), true);
+// Assert.strictEqual(await Publish({
+//     version_name: `Debug Version ${formattedDate}`,
+//     version_number: '1.0.0',
+//     changelog: 'Debug Changelog',
+//     dependencies: '',
+//     game_versions: '1.19, 1.19.1, 1.19.2, 1.19.3, 1.19.4',
+//     version_type: 'release',
+//     loaders: 'fabric',
+//     featured: 'false',
+//     status: 'listed',
+//     project_id: process.env.DEBUG_PROJECT_ID,
+//     file_path: './test/debug.zip',
+// }), true);
